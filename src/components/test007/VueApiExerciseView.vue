7792ef042f3b81205a6495c26f81a6a3a6468d90
linyijin <linyijin@wanda.cn>
2018-05-15 11:36:58 +0800

    添加Element上传图片

--- src/components/test007/VueApiExerciseView.vue
+++ src/components/test007/VueApiExerciseView.vue
@@ -8,7 +8,19 @@
     </div>
     <div class="view-interval child-two-view">
       <h1>vue-html5-editor</h1>
-      <vue-html5-editor :content="content" :height="500"></vue-html5-editor>
+      <vue-html5-editor :content="content" :height="200"></vue-html5-editor>
+    </div>
+    <div class="view-interval child-two-view">
+      <h1>Element-Upload</h1>
+      <el-upload
+        class="upload-demo"
+        drag
+        action="https://jsonplaceholder.typicode.com/posts/"
+        multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
     </div>
   </div>
 </template>
